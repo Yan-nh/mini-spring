8f05f3984dc0077cb7616dae7d3af04c4ec22eca
YZY <yzy442633025@163.com>
2023-12-29 15:29:13 +0800

    step-01：创建简单的bean容器

---
+++ src/main/java/cn/bugstack/springframework/BeanDefinition.java
@@ -0,0 +1,15 @@
+package cn.bugstack.springframework;
+
+public class BeanDefinition {
+
+    private Object bean;
+
+    public BeanDefinition(Object bean) {
+        this.bean = bean;
+    }
+
+    public Object getBean() {
+        return bean;
+    }
+
+}
